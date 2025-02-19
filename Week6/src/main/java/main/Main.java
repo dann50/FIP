35add7b113c2bbd08c0ef152b7068a228d8dd083
Daniel Dominic <ddaniel8612@gmail.com>
2025-02-19 15:55:18 +0100

    Week 6

--- Week6/src/main/java/main/Main.java
+++ Week6/src/main/java/main/Main.java
@@ -9,8 +9,11 @@ public class Main {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
         Person[] persons = new Person[3];
+
         for (int i = 0; i < persons.length; i++) {
             Person person = context.getBean(Person.class);
+
+            // The Beans are annotated with a prototype scope, so it prints a different Person instance each time
             System.out.println(person);
         }
     }
